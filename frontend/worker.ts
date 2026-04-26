b4a7b5fbf1ff435b4fe098e976fa828de2883423
ntkrnl64 <nt@krnl64.win>
2026-04-26 21:12:28 +0800

    add worker files

---
+++ frontend/worker.ts
@@ -0,0 +1,13 @@
+interface Env {
+  ASSETS: { fetch(request: Request): Promise<Response> };
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/api/")) {
+      return fetch(request);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
